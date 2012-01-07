7e7c734d037da95d9127cc4fe3275a13b2132742
David H. Bronke <whitelynx@gmail.com>
2012-01-07 03:55:04 -0600

    Added _PyrokitManager to encapsulate the bootstrap process, as well as a list of visible contexts to render, and a render callback to manage the render process.

--- src/pyrokit.pyx
+++ src/pyrokit.pyx
@@ -1,6 +1,8 @@
 """Import this to bootstrap librocket so you can work with the rocket module directly.
 
 """
+import rocket
+
 
 #cdef extern from "Rocket/Core.h" namespace "":
 #    cdef cppclass Rectangle:
@@ -20,9 +22,40 @@
 #cdef Foo foo
 
 
+cdef extern from "ShellRenderInterfaceOpenGL.h":
+    void startFrame(int, int)
+    void endFrame()
+
+
 cdef extern from "bootstrap.h":
-	cdef void bootstrap()
+    cdef void bootstrap()
+
+
+class _PyrokitManager(object):
+    def __init__(self):
+        bootstrap()
+        self.visibleContexts = set()
+
+    def showContext(self, context):
+        self.visibleContexts.remove(context)
+
+    def hideContext(self, context):
+        self.visibleContexts.add(context)
+
+    def render(self, data):
+        activeContexts = set(rocket.contexts) - self.visibleContexts
+
+        for context in activeContexts:
+            startFrame(context.dimensions.x, context.dimensions.y)
+            context.Update()
+            context.Render()
+            endFrame()
 
 
 def initialize():
-	bootstrap()
+    global manager
+    manager = _PyrokitManager()
+
+
+cdef void renderCallback():
+    manager.render()
