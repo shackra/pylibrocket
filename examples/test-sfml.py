7a4f47f3c91d273cb51b383ee36103fa25525f46
shackra sislock <jorgean@lavabit.com>
2013-02-09 22:04:15 -0600

    working on a better example based on libRocket first tutorial. I have an issue with the property overflow: auto because it hiddes the entire text :S

--- examples/test-sfml.py
+++ examples/test-sfml.py
@@ -24,26 +24,32 @@ def main():
             window.size.x, window.size.y))
     
     #os.chdir(os.path.dirname(__file__))
-    rocket.LoadFontFace("template/data/assets/Delicious-Roman.otf")
-    rocket.LoadFontFace("template/data/assets/Delicious-Bold.otf")
-    rocket.LoadFontFace("template/data/assets/Delicious-BoldItalic.otf")
-    rocket.LoadFontFace("template/data/assets/Delicious-Italic.otf")
-    rocketContext.LoadDocument('template/data/tutorial.rml').Show()
+    rocket.LoadFontFace("data/assets/Delicious-Roman.otf")
+    rocket.LoadFontFace("data/assets/Delicious-Bold.otf")
+    rocket.LoadFontFace("data/assets/Delicious-BoldItalic.otf")
+    rocket.LoadFontFace("data/assets/Delicious-Italic.otf")
+    rocketContext.LoadDocument('data/tutorial.rml').Show()
     
     running = True
     while running:
         for event in window.events:
             # Stop running if the application is closed
             # or if the user presses Escape
-            if type(event) == sfml.CloseEvent or \
-            (type(event) == sfml.KeyEvent and 
-             event.code == sfml.Keyboard.ESCAPE):
+            if type(event) == sfml.CloseEvent:
                 running = False
+
+            elif type(event) == sfml.KeyEvent:
+                if event.code == sfml.Keyboard.F5:
+                    # refresh the document
+                    # HOW?
+                    pass
+                elif event.code == sfml.Keyboard.ESCAPE:
+                    running = False
             else:
                 pylibrocket.sf.processEvent(rocketContext, event)
-        
+
         window.clear(sfml.Color.BLACK)
-        
+
         #window.draw(sprite)
         pylibrocket.manager.render(None)
         
